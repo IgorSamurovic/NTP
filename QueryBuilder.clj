9c45c55ae127599b635679b2566bd23ea82348f7
Deconstruction <igorsamurovic@hotmail.com>
2017-08-06 08:49:30 +0200

    add clojure version of the query builder

---
+++ QueryBuilder.clj
@@ -0,0 +1,121 @@
+(ns querybuilderclojure.clojure.core
+  (:gen-class))
+
+(require 'clojure.string)
+
+(defn chain
+  ([strings]
+   (reduce (fn [current bonus] 
+             (str (name current) ", " (name bonus))) strings))
+  ([strings joiner]
+   (reduce (fn [current bonus] 
+             (str (name current) joiner (name bonus))) strings)))
+
+(defn convertOrderBy [s]
+  (case (.substring s 0 1)
+    "!" (str (.substring s 1 (.length s)) " DESC")
+    (str s " ASC")))
+
+(defn chainOrderBy
+  ([strings]
+   (reduce (fn [current bonus] 
+             (str 
+               (if (.contains (name current) " ") 
+                 (name current)
+                 (convertOrderBy (name current)))
+               ", " (convertOrderBy (name bonus))))
+           strings)))
+
+
+(defn SELECT
+  ;; No arguments
+  ([]
+   (SELECT "*"))
+  ;; Multiple arguments
+  ([& args]
+   (str "SELECT " 
+     (chain args))))
+
+(defn FROM [qry table]
+  (str qry " FROM " table))
+
+(defn parenthesis [string]
+  (str "(" (name string) ")"))
+
+(defn OR [& parts]
+  (-> (chain parts " OR ") parenthesis))
+
+(defn AND [& parts]
+  (-> (chain parts " AND ") parenthesis))
+
+(defn WHERE
+  ;; No additional arguments
+  ([qry]
+   (qry))
+  ([qry args]
+   (str qry " WHERE "  args)))
+
+(defn ON [on]
+  (str " ON " on))
+
+(defn ORDER_BY
+  ([qry]
+   (qry))
+  ([qry & args]
+   (str qry " ORDER BY " 
+     (chainOrderBy args))))
+
+(defn COMPARATOR [a sign b]
+  (str (name a) " " sign " " (name b)))
+
+(defn LIMIT
+  ([qry num]
+   (str qry " LIMIT " num))
+  ([qry num offset]
+   (str qry " LIMIT " num ", " offset)))
+
+(defn EQUAL [a b]
+  (COMPARATOR a "=" b))
+
+(defn NOT_EQUAL [a b]
+  (COMPARATOR a "!=" b))
+
+(defn GREATER [a b]
+  (COMPARATOR a ">" b))
+
+(defn GREATER_OR_EQUAL [a b]
+  (COMPARATOR a ">=" b))
+
+(defn LESSER [a b]
+  (COMPARATOR a "<" b))
+
+(defn LESSER_OR_EQUAL [a b]
+  (COMPARATOR a "<=" b))
+
+(defn LIKE [a b]
+  (COMPARATOR a "LIKE" b))  
+
+(defn JOIN 
+  ([qry joinType table]
+   (str qry " " (-> (name joinType) clojure.string/upper-case)" JOIN " table))
+  ([qry joinType table on]
+   (str (JOIN qry joinType table) on)))
+                              
+
+(defn -main []
+  (println "TEST QUERY: \n")
+  (println (->
+               (SELECT :u.id :v.id :u.username :v.username)
+               (FROM "User u")
+               (JOIN :left "User v" 
+                 (ON
+                  (OR
+                   (EQUAL :u.username :v.username)
+                   (EQUAL :u.email :v.email))))
+               (WHERE 
+                 (NOT_EQUAL :u.id :v.id))
+               (ORDER_BY :!u.id  :u.username)
+               (LIMIT 2))))
+
+
+
